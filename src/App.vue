5432835adf41793e9a6a3b7851eab4555cff8bc5
xlfstar <xlfstar@163.com>
2025-06-16 22:56:50 +0800

    测试部署

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main">
+    <div class="content">dsdsds</div>
+    <HelloWorld />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+ 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.main{
+  background-color: aqua;
+  .content{
+    color:red
+  }
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:process.env.NODE_ENV === 'production' ? '/domi/' : '/',
+  resolve:{
+    alias:{
+      '@': '/src' // 确保别名已配置
+    }
+  }
 })
